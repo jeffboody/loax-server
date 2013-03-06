09a6862c12fc5620fa9370b9f4b1f91fea38b240
Jeff Boody <jeffboody@gmail.com>
2013-03-06 05:19:52 +0000

    add a3d, a3d-fonts, net, loax submodules

--- build-java.sh
+++ build-java.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo `date` > project/res/raw/timestamp.raw
+cp -f fonts/whitrabt/whitrabt.texgz project/res/raw/whitrabt.raw
 ant -f project/build.xml clean
 ant -f project/build.xml debug
